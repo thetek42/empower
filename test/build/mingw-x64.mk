9a443b747d767a53e5f49a20f57710f12972ed42
thetek <git@thetek.de>
2024-11-01 20:36:13 +0100

    fix: mingw support

---
+++ test/build/mingw-x64.mk
@@ -0,0 +1,30 @@
+STDC ?= c2x
+MODE ?= debug
+
+CC     := x86_64-w64-mingw32-gcc
+CFLAGS := -std=$(STDC) -Isrc
+CFLAGS += -Wall -Wextra -Werror -Wdouble-promotion -Wconversion -pedantic
+CFLAGS += -Wno-sign-conversion -Wno-attributes -Wno-stringop-truncation
+CFLAGS += -DDEBUG -Og -ggdb3
+
+IDENT := mingw-x64-$(STDC)
+
+HEADERS := $(wildcard src/*.h)
+TEST_SRC := $(wildcard test/*.c)
+TEST_BIN := bin/$(IDENT)/empower-test.exe
+
+
+all: $(TEST_BIN)
+	@echo -e '\x1b[34mTEST \x1b[0m $(TEST_BIN)'
+	@./$(TEST_BIN)
+
+clean:
+	rm -rf bin/
+
+.PHONY: all clean
+
+
+$(TEST_BIN): $(TEST_SRC) $(HEADERS)
+	@mkdir -p $(@D)
+	@echo -e '\x1b[32mCC   \x1b[0m $(TEST_BIN)'
+	@$(CC) $(CFLAGS) $(TEST_SRC) -o $@
