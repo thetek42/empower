59e815ce8ba0ab84752759170b6bc55846da483b
thetek <git@thetek.de>
2025-05-25 12:00:37 +0200

    refactor(test): streamline e_test impl

--- src/e_test.h
+++ src/e_test.h
@@ -2,12 +2,16 @@
 #define _EMPOWER_TEST_H_
 
 /*! e_test ********************************************************************
- * 
+ *
  * This module provides basic functionality for unit testing using macros. Type
  * inference can be enabled when using C23 for cleaner code.
  *
+ * Dependencies:
+ *  - e_result (optional)
+ *
  * Configuration options:
  *  - E_CONFIG_TEST_TYPE_INFERENCE: Enable type inference in C23
+ *  - E_CONFIG_TEST_NO_E_RESULT: Disable E_Result support
  *
  ******************************************************************************/
 
@@ -18,6 +22,10 @@
 #include <stdlib.h>
 #include <string.h>
 
+#ifndef E_CONFIG_TEST_NO_E_RESULT
+# include <e_result.h>
+#endif /* E_CONFIG_TEST_NO_E_RESULT */
+
 /* public interface ***********************************************************/
 
 struct e_test_result {
@@ -27,30 +35,32 @@ struct e_test_result {
 
 extern struct e_test_result e_global_test;
 
+void e_test_finish (void);
+void (e_test_assert) (const char *name, const char *location, const char *expr, bool value);
+void (e_test_assert_ptr_eq) (const char *name, const char *location, const char *expr, const void *got, const void *want);
+void (e_test_assert_str_eq) (const char *name, const char *location, const char *expr, const char *got, const char *want);
+void (e_test_assert_ptr_aligned) (const char *name, const char *location, const char *expr, const void *ptr, uintptr_t alignment);
+#ifndef E_CONFIG_TEST_NO_E_RESULT
+void (e_test_assert_ok) (const char *name, const char *location, const char *expr, E_Result result);
+void (e_test_assert_err) (const char *name, const char *location, const char *expr, E_Result result);
+#endif /* E_CONFIG_TEST_NO_E_RESULT */
+
+void e_test_priv_print_name (const char *name, const char *location);
+void e_test_priv_print_assert_fn (const char *fn_name);
+
 #define __E_TEST_STRINGIFY_HELPER(x) #x
 #define __E_TEST_STRINGIFY(x) __E_TEST_STRINGIFY_HELPER (x)
 #define __E_TEST_FILE_LINE __FILE__ ":" __E_TEST_STRINGIFY (__LINE__)
-#define __E_TEST_ASSERT_FMT(name) "\x1b[31mfail\x1b[0m \x1b[33m" name "\x1b[0m (" __E_TEST_FILE_LINE ") "
-#define __E_TEST_SPACE "                                                                      "
-#define __E_TEST_PRINT_ASSERT_FN(name) fprintf (stderr, "\x1b[35m%s\x1b[0m ", name)
 
-/**
- * Assert that a condition is truthy and update the global test statistics. If
- * the check fails, an error message will be printed.
- */
-#define e_test_assert(name, expr)                                              \
-	do {                                                                   \
-		if ((expr)) {                                                  \
-			e_global_test.success += 1;                            \
-		} else {                                                       \
-			int p = fprintf (stderr, __E_TEST_ASSERT_FMT (name));  \
-			int l = (int) strlen (__E_TEST_SPACE) - p;             \
-			fprintf (stderr, "%.*s", l, __E_TEST_SPACE);           \
-			__E_TEST_PRINT_ASSERT_FN ("assert");                   \
-			fprintf (stderr, #expr "\n");                          \
-			e_global_test.failure += 1;                            \
-		}                                                              \
-	} while (0)
+#define e_test_assert(name, expr) (e_test_assert) (name, __E_TEST_FILE_LINE, #expr, expr)
+#define e_test_assert_ptr_eq(name, expr, check) (e_test_assert_ptr_eq) (name, __E_TEST_FILE_LINE, #expr, expr, check)
+#define e_test_assert_null(name, expr) e_test_assert_ptr_eq (name, expr, NULL)
+#define e_test_assert_str_eq(name, expr, check) (e_test_assert_str_eq) (name, __E_TEST_FILE_LINE, #expr, expr, check)
+#define e_test_assert_ptr_aligned(name, expr, alignment) (e_test_assert_ptr_aligned) (name, __E_TEST_FILE_LINE, #expr, expr, alignment)
+#ifndef E_CONFIG_TEST_NO_E_RESULT
+# define e_test_assert_ok(name, expr) (e_test_assert_ok) (name, __E_TEST_FILE_LINE, #expr, expr)
+# define e_test_assert_err(name, expr) (e_test_assert_err) (name, __E_TEST_FILE_LINE, #expr, expr)
+#endif /* E_CONFIG_TEST_NO_E_RESULT */
 
 #if __STDC_VERSION__ >= 201112L
 # define __E_TEST_AUTO_FMT(value) _Generic ((value),                           \
@@ -79,11 +89,6 @@ extern struct e_test_result e_global_test;
 	fprintf (stderr, "\x1b[36mfailed\x1b[0m")
 #endif /* __STDC_VERSION__ >= 201112L */
 
-/**
- * Assert that two values equal each other and update the global test
- * statistics. The types of the values are determined automatically. If the
- * check fails, an error message will be printed.
- */
 #define __e_test_assert_eq(name, type, expr, check)                            \
 	do {                                                                   \
 		type result = (expr);                                          \
@@ -91,22 +96,14 @@ extern struct e_test_result e_global_test;
 		if (result == other) {                                         \
 			e_global_test.success += 1;                            \
 		} else {                                                       \
-			int p = fprintf (stderr, __E_TEST_ASSERT_FMT (name));  \
-			int l = (int) strlen (__E_TEST_SPACE) - p;             \
-			fprintf (stderr, "%.*s", l > 0 ? l : 0,                \
-			         __E_TEST_SPACE);                              \
-			__E_TEST_PRINT_ASSERT_FN ("assert_eq");                \
+			e_global_test.failure += 1;                            \
+			e_test_priv_print_name (name, __E_TEST_FILE_LINE);     \
+			e_test_priv_print_assert_fn ("assert_eq");             \
 			__E_TEST_ASSERT_EQ_PRINT_VALUES (expr, result, other); \
 			fprintf (stderr, "\n");                                \
-			e_global_test.failure += 1;                            \
 		}                                                              \
 	} while (0)
 
-/**
- * Assert that two values do not equal each other and update the global test
- * statistics. The types of the values are determined automatically. If the
- * check fails, an error message will be printed.
- */
 #define __e_test_assert_not_eq(name, type, expr, check)                        \
 	do {                                                                   \
 		type result = (expr);                                          \
@@ -114,155 +111,13 @@ extern struct e_test_result e_global_test;
 		if (result != other) {                                         \
 			e_global_test.success += 1;                            \
 		} else {                                                       \
-			int p = fprintf (stderr, __E_TEST_ASSERT_FMT (name));  \
-			int l = (int) strlen (__E_TEST_SPACE) - p;             \
-			fprintf (stderr, "%.*s", l > 0 ? l : 0,                \
-			         __E_TEST_SPACE);                              \
-			__E_TEST_PRINT_ASSERT_FN ("assert_eq");                \
+			e_global_test.failure += 1;                            \
+			e_test_priv_print_name (name, __E_TEST_FILE_LINE);     \
+			e_test_priv_print_assert_fn ("assert_not_eq");         \
 			__E_TEST_ASSERT_EQ_PRINT_VALUES (expr, result, other); \
 			fprintf (stderr, "\n");                                \
-			e_global_test.failure += 1;                            \
 		}                                                              \
 	} while (0)
-
-/**
- * Assert that two pointers equal each other and update the global test
- * statistics. If the check fails, an error message will be printed.
- */
-#define e_test_assert_ptr_eq(name, expr, check)                                \
-	do {                                                                   \
-		const void *result = (expr);                                   \
-		const void *other = (check);                                   \
-		if (result == other) {                                         \
-			e_global_test.success += 1;                            \
-		} else {                                                       \
-			int p = fprintf (stderr, __E_TEST_ASSERT_FMT (name));  \
-			int l = (int) strlen (__E_TEST_SPACE) - p;             \
-			fprintf (stderr, "%.*s", l > 0 ? l : 0,                \
-			         __E_TEST_SPACE);                              \
-			__E_TEST_PRINT_ASSERT_FN ("assert_ptr_eq");            \
-			fprintf (stderr, "%s \x1b[36mgot\x1b[0m %p "           \
-			         "\x1b[36mwant\x1b[0m %p\n", #expr, result,    \
-			         other);                                       \
-			e_global_test.failure += 1;                            \
-		}                                                              \
-	} while (0)
-
-/**
- * Assert that two strings equal each other and update the global test
- * statistics. If the check fails, an error message will be printed.
- */
-#define e_test_assert_str_eq(name, expr, check)                                \
-	do {                                                                   \
-		const char *result = (expr);                                   \
-		const char *other = (check);                                   \
-		if (result == other ||                                         \
-		    (result && other && !strcmp (result, other))) {            \
-			e_global_test.success += 1;                            \
-		} else {                                                       \
-			int p = fprintf (stderr, __E_TEST_ASSERT_FMT (name));  \
-			int l = (int) strlen (__E_TEST_SPACE) - p;             \
-			fprintf (stderr, "%.*s", l > 0 ? l : 0,                \
-			         __E_TEST_SPACE);                              \
-			__E_TEST_PRINT_ASSERT_FN ("assert_str_eq");            \
-			fprintf (stderr, "%s \x1b[36mgot\x1b[0m \"%s\" "       \
-			         "\x1b[36mwant\x1b[0m \"%s\"\n",               \
-			         __E_TEST_STRINGIFY (expr), result, other);    \
-			e_global_test.failure += 1;                            \
-		}                                                              \
-	} while (0)                                                                                
-
-/**
- * Assert that an `E_Result` is `E_OK` and update the global test statistics. If
- * the check fails, an error message will be printed.
- */
-#define e_test_assert_ok(name, expr)                                           \
-	do {                                                                   \
-		E_Result result = (expr);                                      \
-		if (result == E_OK) {                                          \
-			e_global_test.success += 1;                            \
-		} else {                                                       \
-			int p = fprintf (stderr, __E_TEST_ASSERT_FMT (name));  \
-			int l = (int) strlen (__E_TEST_SPACE) - p;             \
-			fprintf (stderr, "%.*s", l > 0 ? l : 0,                \
-			         __E_TEST_SPACE);                              \
-			__E_TEST_PRINT_ASSERT_FN ("assert_ok");                \
-			fprintf (stderr, "%s \x1b[36mreturned error\x1b[0m "   \
-			         "%s\n", #expr, e_result_to_str (result));     \
-			e_global_test.failure += 1;                            \
-		}                                                              \
-	} while (0)
-
-/**
- * Assert that an `E_Result` is not `E_OK` and update the global test
- * statistics. If the check fails, an error message will be printed.
- */
-#define e_test_assert_err(name, expr)                                          \
-	do {                                                                   \
-		E_Result result = (expr);                                      \
-		if (result != E_OK) {                                          \
-			e_global_test.success += 1;                            \
-		} else {                                                       \
-			int p = fprintf (stderr, __E_TEST_ASSERT_FMT (name));  \
-			int l = (int) strlen (__E_TEST_SPACE) - p;             \
-			fprintf (stderr, "%.*s", l > 0 ? l : 0,                \
-			         __E_TEST_SPACE);                              \
-			__E_TEST_PRINT_ASSERT_FN ("assert_ok");                \
-			fprintf (stderr, "%s \x1b[36mreturned ok\x1b[0m\n",    \
-			         #expr);                                       \
-			e_global_test.failure += 1;                            \
-		}                                                              \
-	} while (0)
-
-/**
- * Assert that a pointer is aligned to a certain alignment and update the global
- * test statistics. If the check fails, an error message will be printed.
- */
-#define e_test_assert_ptr_aligned(name, expr, alignment)                       \
-	do {                                                                   \
-		const void *result = (expr);                                   \
-		if (((uintptr_t) ptr % alignment) == 0) {                      \
-			e_global_test.success += 1;                            \
-		} else {                                                       \
-			int p = fprintf (stderr, __E_TEST_ASSERT_FMT (name));  \
-			int l = (int) strlen (__E_TEST_SPACE) - p;             \
-			fprintf (stderr, "%.*s", l > 0 ? l : 0,                \
-			         __E_TEST_SPACE);                              \
-			__E_TEST_PRINT_ASSERT_FN ("assert_ptr_aligned");       \
-			fprintf (stderr, "%s = \x1b[36m%p\x1b[0m not aligned " \
-			         "to %zu bytes\n", #expr, result,              \
-			         (size_t) alignment);                          \
-			e_global_test.failure += 1;                            \
-		}                                                              \
-	} while (0)
-
-/**
- * Finish the tests, print the test statistics, and terminate the programme. The
- * exit code is 0 if all tests succeeded, or non-zero if there were failures.
- */
-#define e_test_finish()                                                        \
-	do {                                                                   \
-		double total_perc = (double) (e_global_test.success +          \
-		                        e_global_test.failure) / 100.0;        \
-		double succ_rate = (double) e_global_test.success / total_perc;\
-		double fail_rate = (double) e_global_test.failure / total_perc;\
-		fprintf (stderr, "\n=======================================\n" \
-		         "TEST SUMMARY:"                                       \
-		         "\n - total:   %zu"                                   \
-			 "\n - \x1b[32msuccess: %zu (%.2f%%)\x1b[0m"           \
-			 "\n - \x1b[31mfailure: %zu (%.2f%%)\x1b[0m"           \
-		         "\n=======================================\n",        \
-			 e_global_test.success + e_global_test.failure,        \
-			 e_global_test.success, succ_rate,                     \
-			 e_global_test.failure, fail_rate);                    \
-		exit (e_global_test.failure > 0 ? EXIT_FAILURE : EXIT_SUCCESS);\
-	} while (0)
-
-/**
- * Assert that a pointer is `nullptr` and update the global test statistics. If
- * the check fails, an error message will be printed.
- */
-#define e_test_assert_null(name, expr) e_test_assert_ptr_eq (name, expr, NULL)
 
 #if __STDC_VERSION__ >= 202000L && E_CONFIG_TEST_TYPE_INFERENCE
 # define e_test_assert_eq(name, expr, check) __e_test_assert_eq (name, auto, expr, check);
@@ -277,6 +132,135 @@ extern struct e_test_result e_global_test;
 #ifdef E_TEST_IMPL
 
 struct e_test_result e_global_test = {0};
+
+void
+e_test_finish (void)
+{
+	double success, failure, total_perc, success_rate, failure_rate;
+
+	success = (double) e_global_test.success;
+	failure = (double) e_global_test.failure;
+	total_perc = (success + failure) / 100.0;
+	success_rate = success / total_perc;
+	failure_rate = failure / total_perc;
+
+	fprintf (stderr,
+	         "\n=======================================\n"
+	         "TEST SUMMARY:"
+	         "\n - total:   %zu"
+		 "\n - \x1b[32msuccess: %zu (%.2f%%)\x1b[0m"
+		 "\n - \x1b[31mfailure: %zu (%.2f%%)\x1b[0m"
+	         "\n=======================================\n",
+		 e_global_test.success + e_global_test.failure,
+		 e_global_test.success, success_rate,
+		 e_global_test.failure, failure_rate);
+
+	exit (e_global_test.failure > 0 ? EXIT_FAILURE : EXIT_SUCCESS);
+}
+
+void
+(e_test_assert) (const char *name, const char *location, const char *expr,
+                 bool value)
+{
+	if (value) {
+		e_global_test.success += 1;
+	} else {
+		e_global_test.failure += 1;
+		e_test_priv_print_name (name, location);
+		e_test_priv_print_assert_fn ("assert");
+		fprintf (stderr, "%s\n", expr);
+	}
+}
+
+void
+(e_test_assert_ptr_eq) (const char *name, const char *location,
+                        const char *expr, const void *got, const void *want)
+{
+	if (got == want) {
+		e_global_test.success += 1;
+	} else {
+		e_global_test.failure += 1;
+		e_test_priv_print_name (name, location);
+		e_test_priv_print_assert_fn ("assert_ptr_eq");
+		fprintf (stderr, "%s \x1b[36mgot\x1b[0m %p "
+		         "\x1b[36mwant\x1b[0m %p\n", expr, got, want);
+	}
+}
+
+void
+(e_test_assert_str_eq) (const char *name, const char *location,
+                        const char *expr, const char *got, const char *want)
+{
+	if (got == want || (got && want && !strcmp (got, want))) {
+		e_global_test.success += 1;
+	} else {
+		e_global_test.failure += 1;
+		e_test_priv_print_name (name, location);
+		e_test_priv_print_assert_fn ("assert_str_eq");
+		fprintf (stderr, "%s \x1b[36mgot\x1b[0m %s "
+		         "\x1b[36mwant\x1b[0m %s\n", expr, got, want);
+	}
+}
+
+void
+(e_test_assert_ptr_aligned) (const char *name, const char *location,
+                             const char *expr, const void *ptr,
+                             uintptr_t alignment)
+{
+	if (((uintptr_t) ptr % alignment) == 0) {
+		e_global_test.success += 1;
+	} else {
+		e_global_test.failure += 1;
+		e_test_priv_print_name (name, location);
+		e_test_priv_print_assert_fn ("assert_ptr_aligned");
+		fprintf (stderr, "%s = \x1b[36m%p\x1b[0m not aligned to "
+		         "%zu bytes\n", expr, ptr, alignment);
+	}
+}
+
+#ifndef E_CONFIG_TEST_NO_E_RESULT
+void
+(e_test_assert_ok) (const char *name, const char *location, const char *expr,
+                    E_Result result)
+{
+	if (result == E_OK) {
+		e_global_test.success += 1;
+	} else {
+		e_global_test.failure += 1;
+		e_test_priv_print_name (name, location);
+		e_test_priv_print_assert_fn ("assert_ok");
+		fprintf (stderr, "%s \x1b[36mreturned error\x1b[0m %s\n",
+		         expr, e_result_to_str (result));
+	}
+}
+
+void
+(e_test_assert_err) (const char *name, const char *location, const char *expr,
+                     E_Result result)
+{
+	if (result != E_OK) {
+		e_global_test.success += 1;
+	} else {
+		e_global_test.failure += 1;
+		e_test_priv_print_name (name, location);
+		e_test_priv_print_assert_fn ("assert_err");
+		fprintf (stderr, "%s \x1b[36mreturned ok\x1b[0m\n", expr);
+	}
+}
+#endif /* E_CONFIG_TEST_NO_E_RESULT */
+
+void
+e_test_priv_print_name (const char *name, const char *location)
+{
+	fprintf (stderr, "\x1b[31mfail\x1b[0m \x1b[33m%s\x1b[0m (%s) ",
+	         name, location);
+}
+
+void
+e_test_priv_print_assert_fn (const char *fn_name)
+{
+	fprintf (stderr, "\x1b[35m%s\x1b[0m ", fn_name);
+}
 
 #endif /* E_TEST_IMPL */
 
