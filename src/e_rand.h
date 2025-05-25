2229173da3780a23b59394549b00d74d4fd45d38
thetek <git@thetek.de>
2025-05-25 16:20:17 +0200

    feat(rand): use xoshiro256++

--- src/e_rand.h
+++ src/e_rand.h
@@ -2,8 +2,11 @@
 #define _EMPOWER_RAND_H_
 
 /*! e_rand ********************************************************************
- * 
- * This module provides functions related to randomization.
+ *
+ * This module provides functions related to randomization. For random number
+ * generation, the xoshiro256++ algorithm is used. When C11 or above is used, a
+ * thread-local PRNG is provided. Note that all PRNGs in this module must be
+ * explicitly seeded by the user, otherwise they will always have the same seed.
  *
  ******************************************************************************/
 
@@ -11,76 +14,76 @@
 
 /* public interface ***********************************************************/
 
-void e_rand_seed (uint64_t seed);
-void e_rand_seed_time (void);
-uint32_t e_rand_u32 (void);
-uint64_t e_rand_u64 (void);
+typedef struct {
+	uint64_t s[4];
+} E_Rand_State;
+
+E_Rand_State e_rand_state_init (void);
+void e_rand_state_seed (E_Rand_State *state, uint64_t seed[4]);
+uint64_t e_rand_state_next (E_Rand_State *state);
+float e_rand_state_f32 (E_Rand_State *state);
+double e_rand_state_f64 (E_Rand_State *state);
+
+#if __STDC_VERSION__ >= 201112L
+void e_rand_seed (uint64_t seed[4]);
+uint64_t e_rand_next (void);
 float e_rand_f32 (void);
 double e_rand_f64 (void);
+#endif /* __STDC_VERSION__ >= 201112L */
 
 /* implementation *************************************************************/
 
 #ifdef E_RAND_IMPL
 
-#include <stdlib.h>
-#include <time.h>
+#if __STDC_VERSION__ >= 201112L
+# include <threads.h>
+static thread_local E_Rand_State e_rand_thread_local_state = {.s = {0, 0, 0, 0}};
+#endif /* __STDC_VERSION__ >= 201112L */
+
+static inline uint64_t e_rand_priv_rotl (uint64_t x, int k);
 
 /**
- * Seed the pseudo-random number source with the value \seed.
+ * Initialise the pseudo-random number generator. It is always initialised to
+ * zero, and you will have to seed it yourself.
+ */
+E_Rand_State
+e_rand_state_init (void)
+{
+	return (E_Rand_State) {
+		.s = {0, 0, 0, 0},
+	};
+}
+
+/**
+ * Seed the pseudo-random number generator \state with \seed.
  */
 void
-e_rand_seed (uint64_t seed)
+e_rand_state_seed (E_Rand_State *state, uint64_t seed[4])
 {
-#if defined (__MINGW32__) || defined (_WIN32) || defined (WIN32)
-	srand ((unsigned int) seed);
-#else /* defined (__MINGW32__) || defined (_WIN32) || defined (WIN32) */
-	srand48 ((long) seed);
-#endif /* defined (__MINGW32__) || defined (_WIN32) || defined (WIN32) */
+	state->s[0] = seed[0];
+	state->s[1] = seed[1];
+	state->s[2] = seed[2];
+	state->s[3] = seed[3];
 }
 
 /**
- * Seed the pseudo-random number source with the result from `time()`. This is
- * not amazing, but works for most cases.
- */
-void
-e_rand_seed_time (void)
-{
-	e_rand_seed ((uint64_t) time (NULL));
-}
-
-/**
- * Obtain a pseudo-random 32-bit number.
- */
-uint32_t
-e_rand_u32 (void)
-{
-#if defined (__MINGW32__) || defined (_WIN32) || defined (WIN32)
-# if RAND_MAX != 0x7FFF
-#  error "RAND_MAX is expected to be 0x7FFF on Windows."
-# endif /* RAND_MAX != 0x7FFF */
-	return ((uint32_t) rand () << 30) | ((uint32_t) rand () << 15) |
-	       ((uint32_t) rand ());
-#else /* defined (__MINGW32__) || defined (_WIN32) || defined (WIN32) */
-	return (uint32_t) mrand48 ();
-#endif /* defined (__MINGW32__) || defined (_WIN32) || defined (WIN32) */
-}
-
-/**
- * Obtain a pseudo-random 64-bit number.
+ * Obtain the next value from the pseudo-random number generator \state.
  */
 uint64_t
-e_rand_u64 (void)
+e_rand_state_next (E_Rand_State *state)
 {
-#if defined (__MINGW32__) || defined (_WIN32) || defined (WIN32)
-	return ((uint64_t) rand () << 60) | ((uint64_t) rand () << 45) |
-	       ((uint64_t) rand () << 30) | ((uint64_t) rand () << 15) |
-	       ((uint64_t) rand ());
-#else /* defined (__MINGW32__) || defined (_WIN32) || defined (WIN32) */
-	uint64_t a, b;
-	a = (uint64_t) (uint32_t) mrand48 ();
-	b = (uint64_t) (uint32_t) mrand48 ();
-	return (a << 32) | b;
-#endif /* defined (__MINGW32__) || defined (_WIN32) || defined (WIN32) */
+	uint64_t result, t;
+
+	result = e_rand_priv_rotl (state->s[0] + state->s[3], 23) + state->s[0];
+	t = state->s[1] << 17;
+	state->s[2] ^= state->s[0];
+	state->s[3] ^= state->s[1];
+	state->s[1] ^= state->s[2];
+	state->s[0] ^= state->s[3];
+	state->s[2] ^= t;
+	state->s[3] = e_rand_priv_rotl (state->s[3], 45);
+
+	return result;
 }
 
 /**
@@ -88,13 +91,9 @@ e_rand_u64 (void)
  * (inclusive) and 1.0f (exclusive).
  */
 float
-e_rand_f32 (void)
+e_rand_state_f32 (E_Rand_State *state)
 {
-#if defined (__MINGW32__) || defined (_WIN32) || defined (WIN32)
-	return ((float) rand ()) * (1.0f / RAND_MAX);
-#else /* defined (__MINGW32__) || defined (_WIN32) || defined (WIN32) */
-	return (float) drand48 ();
-#endif /* defined (__MINGW32__) || defined (_WIN32) || defined (WIN32) */
+	return ((float) e_rand_state_next (state)) * (1.0f / (float) UINT64_MAX);
 }
 
 /**
@@ -102,13 +101,57 @@ e_rand_f32 (void)
  * (inclusive) and 1.0 (exclusive).
  */
 double
+e_rand_state_f64 (E_Rand_State *state)
+{
+	return ((double) e_rand_state_next (state)) * (1.0 / (double) UINT64_MAX);
+}
+
+#if __STDC_VERSION__ >= 201112L
+
+/**
+ * Seed the thread-local pseudo-random number generator with \seed.
+ */
+void
+e_rand_seed (uint64_t seed[4])
+{
+	e_rand_state_seed (&e_rand_thread_local_state, seed);
+}
+
+/**
+ * Obtain the next value from the thread-local pseudo-random number generator.
+ */
+uint64_t
+e_rand_next (void)
+{
+	return e_rand_state_next (&e_rand_thread_local_state);
+}
+
+/**
+ * Obtain a single-precision floating point number between 0.0f (inclusive) and
+ * 1.0f (exclusive) from the thread-local pseudo-random number generator.
+ */
+float
+e_rand_f32 (void)
+{
+	return e_rand_state_f32 (&e_rand_thread_local_state);
+}
+
+/**
+ * Obtain a double-precision floating point number between 0.0 (inclusive) and
+ * 1.0 (exclusive) from the thread-local pseudo-random number generator.
+ */
+double
 e_rand_f64 (void)
 {
-#if defined (__MINGW32__) || defined (_WIN32) || defined (WIN32)
-	return ((double) rand ()) * (1.0 / RAND_MAX);
-#else /* defined (__MINGW32__) || defined (_WIN32) || defined (WIN32) */
-	return drand48 ();
-#endif /* defined (__MINGW32__) || defined (_WIN32) || defined (WIN32) */
+	return e_rand_state_f64 (&e_rand_thread_local_state);
+}
+
+#endif /* __STDC_VERSION__ >= 201112L */
+
+static inline uint64_t
+e_rand_priv_rotl (uint64_t x, int k)
+{
+	return (x << k) | (x >> (64 - k));
 }
 
 #endif /* E_RAND_IMPL */
